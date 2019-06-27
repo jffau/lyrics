5017f0a8942fc57704b23074f7ff62f9e92b9eff
jeffau13 <jeffau13@gmail.com>
2019-06-27 18:44:52 -0400

    displaying album info

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
 export default class Lyrics extends Component {
   // component level state, not needed in app level state of context
   state = {
@@ -14,17 +16,52 @@ export default class Lyrics extends Component {
           this.props.match.params.id
         }&apikey=${process.env.REACT_APP_MM_KEY}`
       )
-      .then(res =>
-        this.setState({ lyrics: res.data.message.body.lyrics.lyrics_body })
-      )
+      .then(res => {
+        this.setState({ lyrics: res.data.message.body.lyrics.lyrics_body });
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${
+            this.props.match.params.id
+          }&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
+      })
+      .then(res => {
+        this.setState({ track: res.data.message.body.track });
+      })
       .catch(res => console.log(res));
   }
   render() {
-    return (
-      <div>
-        <h1>Lyrics</h1>
-        <p>{this.state.lyrics}</p>
-      </div>
-    );
+    const { track, lyrics } = this.state;
+
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by{' '}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics}</p>
+            </div>
+          </div>
+
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              Album Name: <strong>{track.album_name}</strong>
+            </li>
+          </ul>
+        </>
+      );
+    }
   }
 }
