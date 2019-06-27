8a888796c98b7c5a939da53f076d3a56883d653a
jeffau13 <jeffau13@gmail.com>
2019-06-27 15:01:01 -0400

    fetching data from the musixmatch api with axios

--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 const Context = React.createContext();
 
 export class Provider extends Component {
@@ -18,6 +18,20 @@ export class Provider extends Component {
     ],
     heading: `Top 10 Tracks ${Date.now()}`
   };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`
+      )
+      .then(res => {
+        console.log(res.data);
+        this.setState({ track_list: res.data.message.body.track_list });
+      })
+      .catch();
+  }
 
   render() {
     return (
