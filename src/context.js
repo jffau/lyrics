4ee62fc143b0da34e8ae7f610ff81c6935d783b3
jeffau13 <jeffau13@gmail.com>
2019-06-27 15:05:26 -0400

    added loading spinner graphic

--- src/context.js
+++ src/context.js
@@ -4,18 +4,7 @@ const Context = React.createContext();
 
 export class Provider extends Component {
   state = {
-    track_list: [
-      {
-        track: {
-          track_name: 'my song'
-        }
-      },
-      {
-        track: {
-          track_name: 'song2'
-        }
-      }
-    ],
+    track_list: [],
     heading: `Top 10 Tracks ${Date.now()}`
   };
 
