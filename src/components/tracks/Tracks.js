6abea01b4f104f108548dd6142f65d688087de66
jeffau13 <jeffau13@gmail.com>
2019-06-27 16:01:50 -0400

    displaying track info

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
 import Spinner from '../layout/Spinner';
-
+import Track from './Track';
 export default class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
-          const { track_list } = value;
+          const { track_list, heading } = value;
           if (track_list === undefined || track_list.length === 0) {
             return <Spinner />;
           } else {
-            return <h1>loaded</h1>;
+            return (
+              <>
+                {' '}
+                <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                  {track_list.map(item => {
+                    return (
+                      <Track key={item.track.track_id} track={item.track} />
+                    );
+                  })}
+                </div>
+              </>
+            );
           }
         }}
       </Consumer>
--- src/context.js
+++ src/context.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 const Context = React.createContext();
-
+const today = new Date().toLocaleDateString();
 export class Provider extends Component {
   state = {
     track_list: [],
-    heading: `Top 10 Tracks ${Date.now()}`
+    heading: `Top 50 Tracks for ${today}`
   };
 
   componentDidMount() {
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=50&country=us&f_has_lyrics=1&apikey=${
           process.env.REACT_APP_MM_KEY
         }`
       )
